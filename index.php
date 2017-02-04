b60a4fd78f55f1ed74537e08f5b4f427fd28d3d4
adamparish <asparish@gmail.com>
2017-02-04 18:17:51 +0000

    concatenated the options variable to make it easier to read

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
 <?php
 
-// http://www.esvapi.org/v2/rest/dailyVerse?key=IP&include-audio-link=false&include-short-copyright=false&output-format=plain-text&include-passage-horizontal-lines=false&include-verse-numbers=false
-
   $key = "IP";
-  $passage = urlencode("john 3:16");
-  $options = "include-passage-references=false";
-  $url = "http://www.esvapi.org/v2/rest/dailyverse?key=$key&$options";
+  $options = "include-audio-link=false&";
+  $options .= "include-short-copyright=false&";
+  $options .= "output-format=plain-text&";
+  $options .= "include-passage-horizontal-lines=false&";
+  $options .= "include-verse-numbers=false&";
+  $options .= "include-short-copyright=false&";
+  $options .= "output-format=plain-text&";
+  $options .= "include-footnotes=false";
+  $url = "http://www.esvapi.org/v2/rest/dailyVerse?key=$key&$options";
   $ch = curl_init($url); 
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
   $response = curl_exec($ch);
   curl_close($ch);
   print $response;
+  
 ?>
