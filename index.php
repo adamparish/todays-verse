d81bae558a2660939a6e075b0597652e54245a55
adamparish <asparish@gmail.com>
2017-02-04 17:44:59 +0000

    added dailypassage url

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+
+// http://www.esvapi.org/v2/rest/dailyVerse?key=IP&include-audio-link=false&include-short-copyright=false&output-format=plain-text&include-passage-horizontal-lines=false&include-verse-numbers=false
+
   $key = "IP";
   $passage = urlencode("john 3:16");
   $options = "include-passage-references=false";
-  $url = "http://www.esvapi.org/v2/rest/passageQuery?key=$key&passage=$passage&$options";
+  $url = "http://www.esvapi.org/v2/rest/dailyverse?key=$key&$options";
   $ch = curl_init($url); 
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
   $response = curl_exec($ch);
